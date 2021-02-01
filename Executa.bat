636e6462fff31b86ee7037ae6cd71e32779f86d2
4lexandr3 <52467817+4lexandr3@users.noreply.github.com>
2021-02-01 16:54:45 -0300

    Primeira subida, v5.

---
+++ Executa.bat
@@ -0,0 +1,26 @@
+@echo off
+ECHO.
+ECHO Extraindo dados hist¢ricos.
+rem winrar x -o COTAHIST_A2021.ZIP
+7z e -y COTAHIST_A2021.ZIP
+ECHO Conclu¡do!
+
+python "Carregando Dados B3 em banco de dados.py"
+ECHO.
+set /p resp=Excluir arquivo zip? (S/N) 
+IF '%resp%'=='s' GOTO excluir
+IF '%resp%'=='S' GOTO excluir
+IF NOT '%resp%'=='s' GOTO manter
+
+:excluir
+del COTAHIST_A2021.ZIP
+ECHO Arquivo exclu¡do!
+ECHO.
+GOTO fim
+
+:manter
+ECHO Arquivo mantido!
+ECHO.
+
+:fim
+PAUSE
